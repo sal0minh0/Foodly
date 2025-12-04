0e1cb97f229e375acd6549af9c7761b2d26c2a0d
sal0minh0 <salomaomoraes1@hotmail.com>
2025-12-04 09:23:00 -0300

    fix: para dominios variáveis como o aws ec2

--- src/main/resources/static/WEB/config/api.js
+++ src/main/resources/static/WEB/config/api.js
@@ -1,6 +1,7 @@
 const CONFIG = {
   API_URL:
-    window.location.hostname === "localhost"
+    window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1"
       ? "http://localhost:8080/api"
-      : "http://ec2-18-118-29-99.us-east-2.compute.amazonaws.com:8080/api",
+      : `http://${window.location.hostname}:8080/api`,
 };
